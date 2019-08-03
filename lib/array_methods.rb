49d87bcea97aa7875d97fecc43b1e8bda4a4dc99
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-03 21:59:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,6 +14,7 @@ def find_max_value(array)
       max = array[i]
     end
   end
+  return max
 end
 
 def find_min_value(array)
@@ -23,4 +24,5 @@ def find_min_value(array)
       min = array[i]
     end
   end
+  return min
 end
